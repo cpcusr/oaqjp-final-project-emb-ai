f39d84b0823ebc489669ac65cc0befb79d6f4cd5
cpcusr <claudiu.pantea.cristov@gmail.com>
2024-10-02 00:27:53 -0400

    task7

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,23 +7,32 @@ def emotion_detector(text_to_analyse):
     myobj = { "raw_document": { "text": text_to_analyse } }
 
     response = requests.post(url, json = myobj, headers=header)
-    formatted_response = json.loads(response.text)
-    emotion_prediction = formatted_response["emotionPredictions"][0]["emotion"]
-    
-    dom_emotion_score = 0
-    dom_emotion = ""
-    for key in emotion_prediction:
-        if (emotion_prediction[key] > dom_emotion_score):
-            dom_emotion_score = emotion_prediction[key]
-            dom_emotion = key
 
-    output = {
-        "anger": emotion_prediction["anger"],
-        "disgust": emotion_prediction["disgust"],
-        "fear": emotion_prediction["fear"],
-        "joy": emotion_prediction["joy"],
-        "sadness": emotion_prediction["sadness"],
-        "dominant_emotion": dom_emotion
-    }
+    if response.status_code == 400:
+        return {
+            "anger": None,
+            "disgust": None,
+            "fear": None,
+            "joy": None,
+            "sadness": None,
+            "dominant_emotion": None
+        }
+    elif response.status_code == 200:
+        formatted_response = json.loads(response.text)
+        emotion_prediction = formatted_response["emotionPredictions"][0]["emotion"]
 
-    return output
+        dom_emotion_score = 0
+        dom_emotion = ""
+        for key in emotion_prediction:
+            if (emotion_prediction[key] > dom_emotion_score):
+                dom_emotion_score = emotion_prediction[key]
+                dom_emotion = key
+
+        return {
+            "anger": emotion_prediction["anger"],
+            "disgust": emotion_prediction["disgust"],
+            "fear": emotion_prediction["fear"],
+            "joy": emotion_prediction["joy"],
+            "sadness": emotion_prediction["sadness"],
+            "dominant_emotion": dom_emotion
+        }
