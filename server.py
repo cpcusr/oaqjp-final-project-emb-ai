19d9e790ddc1ab0c95b8159f03b9906c3b511a74
cpcusr <claudiu.pantea.cristov@gmail.com>
2024-10-02 00:44:36 -0400

    task8

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+'''
+Starts the web app deployed on localhost:5000 using Flask
+'''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,20 +8,26 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def analyzer():
+    '''
+    Route accessed from the frontend via GET method, evaluates the emotion
+    and returns HTML containing the result of that evaluation
+    '''
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
 
     if response["dominant_emotion"] is None:
         return "Invalid text! Please try again!"
-    else:
-        return "For the given statement, the system response is 'anger': {}, \
-        'disgust': {}, 'fear': {}, 'joy': {}, and sadness: {}. The dominant \
-        emotion is <b>{}</b>".format(
-            response["anger"], response["disgust"], response["fear"],
-            response["joy"], response["sadness"], response["dominant_emotion"])
+
+    return f"For the given statement, the system response is 'anger': {response['anger']}, \
+    'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']}, \
+    and sadness: {response['sadness']}. The dominant \
+    emotion is <b>{response['dominant_emotion']}</b>"
 
 @app.route("/")
 def render_index_page():
+    '''
+    Display the mai page of the web app
+    '''
     return render_template('index.html')
 
 if __name__ == "__main__":
